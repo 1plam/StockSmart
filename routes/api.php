840ee9926843baa45649e7b86cb02d230e4a29dd
1plam <77026826+1plam@users.noreply.github.com>
2025-01-21 00:47:35 +0100

    feat(api): Set up project routing

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,27 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Presentation\Http\Controllers\{CheckoutController, OrderController, ProductController, UserController};
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// No authenticated routes due to restriction of installing packages, e.g. Sanctum needed for Token management.
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('orders')->group(function () {
+    Route::get('/', [OrderController::class, 'index']);
+    Route::get('/{id}', [OrderController::class, 'show']);
+    Route::post('/', [OrderController::class, 'store']);
+    Route::put('/{id}', [OrderController::class, 'update']);
+    Route::patch('/{id}/status', [OrderController::class, 'updateStatus']);
+    Route::delete('/{id}', [OrderController::class, 'destroy']);
 });
+
+Route::prefix('products')->group(function () {
+    Route::get('/', [ProductController::class, 'index']);
+    Route::get('/{id}', [ProductController::class, 'show']);
+});
+
+Route::prefix('auth')->group(function () {
+    Route::post('/register', [UserController::class, 'register']);
+    Route::post('/login', [UserController::class, 'login']);
+});
+
+Route::get('/profile', [UserController::class, 'profile']);
